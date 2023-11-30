ededb4070c4791bc8c7755f40bf3177eedfdabcd
CtrlTropper <149997788+CtrlTropper@users.noreply.github.com>
2023-11-30 14:16:25 +0700

    formTHCV_ver1.2

--- QuanLyPhanCongCongViec/frmQLThucHienCT.Designer.cs
+++ QuanLyPhanCongCongViec/frmQLThucHienCT.Designer.cs
@@ -46,6 +46,8 @@
             this.btnUpdate = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.picUpload = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picUpload)).BeginInit();
             this.SuspendLayout();
@@ -67,6 +69,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.picUpload);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.btnUpdate);
@@ -275,6 +279,32 @@
             this.picUpload.TabIndex = 19;
             this.picUpload.TabStop = false;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label7.Font = new System.Drawing.Font("Sitka Small Semibold", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(492, 157);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label7.Size = new System.Drawing.Size(108, 19);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "(VD:05:00:00)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label8.Font = new System.Drawing.Font("Sitka Small Semibold", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(492, 208);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label8.Size = new System.Drawing.Size(108, 19);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "(VD:05:00:00)";
+            // 
             // frmQLThucHienCT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -313,5 +343,7 @@
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox picUpload;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
     }
 }
