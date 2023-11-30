22c71b8793bcad893f7989d839824ced06881bed
CtrlTropper <149997788+CtrlTropper@users.noreply.github.com>
2023-11-30 12:34:40 +0700

    bat dau 1

--- QuanLyPhanCongCongViec/frmDangNhap.cs
+++ QuanLyPhanCongCongViec/frmDangNhap.cs
@@ -15,7 +15,7 @@ namespace QuanLyPhanCongCongViec
 {
     public partial class frmDangNhap : Form
     {
-        private const string ConnectionString = "Data Source=DESKTOP-8I1VIDD\\LINHLINH;Initial Catalog=QuanLyCongViec;Integrated Security=True";
+        private const string ConnectionString = "Data Source=DESKTOP-O0D76JD\\PHUONG_DB;Initial Catalog=QuanLyCongViec;Integrated Security=True";
         private SqlConnection connection;
         public frmDangNhap()
         {
